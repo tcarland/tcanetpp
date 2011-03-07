2db0a2825c6fe2699b690e16d70bae29014b9304
Timothy Arland <tcarland@gmail.com>
2011-03-07 13:19:52 -0800

    - finish move of the base heirarchical tree to tcanetpp

---
+++ include/HeirarchicalStringTree.hpp
@@ -0,0 +1,145 @@
+#ifndef _TCANETPP_HEIRARCHICALSTRINGTREE_HPP_
+#define _TCANETPP_HEIRARCHICALSTRINGTREE_HPP_
+
+#include <string>
+#include <map>
+#include <list>
+#include <vector>
+#include <stdexcept>
+
+
+namespace tcanetpp {
+
+
+template < typename ValueType >
+class HeirarchicalStringTreeNode {
+  public:
+
+    typedef HeirarchicalStringTreeNode<ValueType>   TreeNode;
+    typedef std::map<std::string, TreeNode*>        TreeNodeMap;
+    typedef typename TreeNodeMap::iterator          TreeNodeMapIter;
+
+  public:
+
+    HeirarchicalStringTreeNode ( const std::string & nodeName,
+                                 TreeNode          * parent,
+                                 char                delimiter = '/' );
+
+    virtual ~HeirarchicalStringTreeNode();
+
+
+    const std::string&   getName() const;
+    std::string          getAbsoluteName() const;
+
+    ValueType&           getValue();
+    const ValueType&     getValue() const;
+
+    TreeNode*            getParent();
+    const TreeNode*      getParent() const;
+
+    TreeNodeMap&         getChildren();
+    const TreeNodeMap&   getChildren() const;
+
+  private:
+
+    ValueType           _value;
+    std::string         _nodeName;
+    TreeNode *          _parent;
+    char                _delim;
+    TreeNodeMap         _children;
+
+};
+
+
+template < typename ValueType >
+class HeirarchicalStringTree {
+
+  public:
+
+    typedef HeirarchicalStringTreeNode<ValueType>   Node;
+    typedef typename Node::TreeNodeMap              NodeMap;
+    typedef typename Node::TreeNodeMapIter          NodeMapIter;
+
+    typedef std::vector<std::string>                StringList;
+    typedef typename StringList::iterator           StringListIter;
+    typedef std::list<Node*>                        BranchNodeList;
+    typedef typename BranchNodeList::iterator       BranchNodeListIter;
+
+  public:
+
+    HeirarchicalStringTree ( char delimiter = '/' );
+    HeirarchicalStringTree ( const HeirarchicalStringTree & tree );
+
+    virtual ~HeirarchicalStringTree();
+
+
+    HeirarchicalStringTree&  operator= ( const HeirarchicalStringTree & tree );
+
+
+    char            getDelimiter() const;
+    NodeMap&        getRoots();
+    const NodeMap&  getRoots() const;
+
+
+    Node*           find   ( const std::string & absoluteName );
+
+
+    template<typename OutputIterator_>
+    Node*           insert ( const std::string & absoluteName,
+                             OutputIterator_ outIter )
+        throw ( std::runtime_error );
+
+
+    template<typename OutputIterator_>
+    bool            erase  ( const std::string & absoluteName,
+                             OutputIterator_ outIter );
+
+    template<typename OutputIterator_>
+    bool            erase  ( Node * node,
+                             OutputIterator_ outIter );
+
+    int             size()  const;
+    bool            empty() const;
+    void            clear();
+
+
+    template<typename Predicate_>
+    void            depthFirstTraversal ( Node       * node,
+                                          Predicate_ & predicate );
+
+
+    template<typename BranchIterator_, typename OutputIterator_>
+    bool            branchToNodes   ( BranchIterator_  begin,
+                                      BranchIterator_  end,
+                                      OutputIterator_  outIter );
+
+  public:
+
+    struct BreadthOrderingFunctor
+    {
+        BranchNodeList  nodes;
+        void operator() ( Node * node ) { nodes.push_front(node); }
+    };
+
+    struct DepthOrderingFunctor
+    {
+        BranchNodeList  nodes;
+        void operator() ( Node * node ) { nodes.push_back(node); }
+    };
+
+
+  private:
+
+    NodeMap             _roots;
+    char                _delim;
+    int                 _size;
+};
+
+
+}  // namespace
+
+#include "../src/HeirarchicalStringTree.cpp"
+
+
+#endif // _TCANETPP_HEIRARCHICALSTRINGTREE_HPP_
+
---
+++ src/HeirarchicalStringTree.cpp
@@ -0,0 +1,354 @@
+#ifndef _TCANETPP_HEIRARCHICALSTRINGTREE_CPP_
+#define _TCANETPP_HEIRARCHICALSTRINGTREE_CPP_
+
+#include "StringUtils.h"
+
+
+namespace tcanetpp {
+
+
+//-------------------------------------------------------------//
+//  HeirarchicalStringTreeNode
+
+template<typename ValueType>
+HeirarchicalStringTreeNode<ValueType>::HeirarchicalStringTreeNode ( const std::string & nodeName,
+                                                                    TreeNode * parent,
+                                                                    char delimiter )
+    : _nodeName(nodeName),
+      _parent(parent),
+      _delim(delimiter)
+{}
+
+
+template<typename ValueType>
+HeirarchicalStringTreeNode<ValueType>::~HeirarchicalStringTreeNode()
+{}
+
+
+template<typename ValueType>
+const std::string&
+HeirarchicalStringTreeNode<ValueType>::getName() const
+{
+    return _nodeName;
+}
+
+
+template<typename ValueType>
+std::string
+HeirarchicalStringTreeNode<ValueType>::getAbsoluteName() const
+{
+    std::string  absname = _nodeName;
+    TreeNode   * next    = _parent;
+
+    while ( next != NULL ) {
+        absname.insert(absname.begin(), _delim);
+        absname.insert(0, next->getName());
+        next = next->getParent();
+    }
+
+    return absname;
+}
+
+
+template<typename ValueType>
+ValueType&
+HeirarchicalStringTreeNode<ValueType>::getValue()
+{
+    return _value;
+}
+
+
+template<typename ValueType>
+const ValueType&
+HeirarchicalStringTreeNode<ValueType>::getValue() const
+{
+    return _value;
+}
+
+
+template<typename ValueType>
+typename HeirarchicalStringTreeNode<ValueType>::TreeNode*
+HeirarchicalStringTreeNode<ValueType>::getParent()
+{
+    return _parent;
+}
+
+
+template<typename ValueType>
+const typename HeirarchicalStringTreeNode<ValueType>::TreeNode*
+HeirarchicalStringTreeNode<ValueType>::getParent() const
+{
+    return _parent;
+}
+
+
+template<typename ValueType>
+typename HeirarchicalStringTreeNode<ValueType>::TreeNodeMap&
+HeirarchicalStringTreeNode<ValueType>::getChildren()
+{
+    return _children;
+}
+
+
+template<typename ValueType>
+const typename HeirarchicalStringTreeNode<ValueType>::TreeNodeMap&
+HeirarchicalStringTreeNode<ValueType>::getChildren() const
+{
+    return _children;
+}
+
+//-------------------------------------------------------------//
+//  HeirarchicalStringTree
+
+
+template<typename ValueType>
+HeirarchicalStringTree<ValueType>::HeirarchicalStringTree ( char delimiter )
+    : _delim(delimiter),
+      _size(0)
+{}
+
+
+template<typename ValueType>
+HeirarchicalStringTree<ValueType>::HeirarchicalStringTree ( const HeirarchicalStringTree & tree )
+{}
+
+
+template<typename ValueType>
+HeirarchicalStringTree<ValueType>::~HeirarchicalStringTree()
+{
+    this->clear();
+}
+
+
+template<typename ValueType>
+typename HeirarchicalStringTree<ValueType>::HeirarchicalStringTree&
+HeirarchicalStringTree<ValueType>::operator= ( const HeirarchicalStringTree & tree )
+{}
+
+
+template<typename ValueType>
+char
+HeirarchicalStringTree<ValueType>::getDelimiter() const
+{
+    return _delim;
+}
+
+
+template<typename ValueType>
+typename HeirarchicalStringTree<ValueType>::NodeMap&
+HeirarchicalStringTree<ValueType>::getRoots()
+{
+    return _roots;
+}
+
+
+template<typename ValueType>
+const typename HeirarchicalStringTree<ValueType>::NodeMap&
+HeirarchicalStringTree<ValueType>::getRoots() const
+{
+    return _roots;
+}
+
+
+template<typename ValueType>
+int
+HeirarchicalStringTree<ValueType>::size() const
+{
+    return _size;
+}
+
+
+template<typename ValueType>
+bool
+HeirarchicalStringTree<ValueType>::empty() const
+{
+    return(_size == 0);
+}
+
+
+template<typename ValueType>
+typename HeirarchicalStringTree<ValueType>::Node*
+HeirarchicalStringTree<ValueType>::find ( const std::string & absoluteName )
+{
+    StringList      branchNames;
+    BranchNodeList  branches;
+
+    StringUtils::split(absoluteName, _delim, std::back_inserter(branchNames));
+
+    this->branchToNodes(branchNames.begin(), branchNames.end(),
+                        std::back_inserter(branches));
+
+    if ( branches.empty() || branches.size() != branchNames.size() )
+        return NULL;
+
+    return branches.back();
+}
+
+
+template<typename ValueType>
+template<typename OutputIterator_>
+typename HeirarchicalStringTree<ValueType>::Node*
+HeirarchicalStringTree<ValueType>::insert ( const std::string & absoluteName,
+                                            OutputIterator_     outIter )
+    throw ( std::runtime_error )
+{
+    StringList      branchNames;
+    BranchNodeList  branches;
+
+    StringUtils::split(absoluteName, _delim, std::back_inserter(branchNames));
+
+    if ( branchNames.empty() )
+        return NULL;
+
+    if ( this->branchToNodes(branchNames.begin(), branchNames.end(), std::back_inserter(branches)) )
+        return NULL;
+
+    NodeMap * children = &_roots;
+    Node    * parent   = NULL;
+    Node    * node     = NULL;
+
+    if ( ! branches.empty() ) {
+        children = &branches.back()->getChildren();
+        parent   = branches.back();
+    }
+
+    StringList::size_type  bi;
+    NodeMapIter      nIter;
+
+    for ( bi = branches.size(); bi < branchNames.size(); ++bi ) {
+        std::pair<NodeMapIter, bool> insertR = children->insert(typename NodeMap::value_type(branchNames[bi], 0));
+
+        if ( ! insertR.second )
+            throw std::runtime_error("insert failed on " + absoluteName);
+
+        nIter = insertR.first;
+        node  = new Node(nIter->first, parent);
+
+        nIter->second = node;
+        *outIter++    = node;
+        ++_size;
+
+        parent   = node;
+        children = &node->getChildren();
+    }
+
+    return node;
+}
+
+
+template<typename ValueType>
+template<typename OutputIterator_>
+bool
+HeirarchicalStringTree<ValueType>::erase ( const std::string & absoluteName,
+                                           OutputIterator_     outIter )
+{
+    Node * node = this->find(absoluteName);
+    return( this->erase(node, outIter) );
+}
+
+
+template<typename ValueType>
+template<typename OutputIterator_>
+bool
+HeirarchicalStringTree<ValueType>::erase ( Node  * node,
+                                           OutputIterator_ outIter )
+{
+    if ( node == NULL )
+        return false;
+
+    DepthOrderingFunctor   depthfirst;
+    const std::string      name       = node->getName();
+    NodeMap              * pChildList = &_roots;
+
+    if ( node->getParent() != NULL )
+        pChildList = &node->getParent()->getChildren();
+
+
+    this->depthFirstTraversal(node, depthfirst);
+    _size -= depthfirst.nodes.size();
+
+    BranchNodeList & branches = depthfirst.nodes;
+    BranchNodeListIter bIter;
+
+    for ( bIter = branches.begin(); bIter != branches.end(); ++bIter ) {
+        *outIter++ = (*bIter)->getAbsoluteName();
+        delete *bIter;
+    }
+
+    pChildList->erase(name);
+
+    return true;
+}
+
+
+template<typename ValueType>
+void
+HeirarchicalStringTree<ValueType>::clear()
+{
+    NodeMapIter  nIter;
+
+    for ( nIter = _roots.begin(); nIter != _roots.end(); ++nIter ) {
+        DepthOrderingFunctor  depthfirst;
+        BranchNodeList      & branches = depthfirst.nodes;
+        BranchNodeListIter    bIter;
+
+        this->depthFirstTraversal(nIter->second, depthfirst);
+        _size -= depthfirst.nodes.size();
+
+        for ( bIter = branches.begin(); bIter != branches.end(); ++bIter )
+            delete *bIter;
+    }
+    _roots.clear();
+
+    return;
+}
+
+
+template<typename ValueType>
+template<typename BranchIterator_, typename OutputIterator_>
+bool
+HeirarchicalStringTree<ValueType>::branchToNodes ( BranchIterator_  begin,
+                                                   BranchIterator_  end,
+                                                   OutputIterator_  outIter )
+{
+    NodeMap *  children = &_roots;
+
+    for ( ; begin != end; ++begin )
+    {
+        NodeMapIter nIter;
+
+        if ( (nIter = children->find(*begin)) == children->end() )
+            return false;
+
+        *outIter++ = nIter->second;
+        children   = &nIter->second->getChildren();
+    }
+
+    return true;
+}
+
+
+template<typename ValueType>
+template<typename Predicate_>
+void
+HeirarchicalStringTree<ValueType>::depthFirstTraversal ( Node * node, Predicate_ & predicate )
+{
+    if ( node == NULL )
+        return;
+
+    NodeMap & children = node->getChildren();
+
+    NodeMapIter  nIter;
+    for ( nIter = children.begin(); nIter != children.end(); ++nIter )
+        this->depthFirstTraversal(nIter->second, predicate);
+
+    predicate(node);
+
+    return;
+}
+
+
+} // namespace
+
+#endif // _TCANETPP_HEIRARCHICALSTRINGTREE_CPP_
+
